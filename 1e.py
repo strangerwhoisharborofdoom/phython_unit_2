3c30f940ff3de19b0fa71b3ad00d2b511a8c8eaa
Pavan C N <p0073100@gmail.com>
2026-06-04 14:33:09 +0530

    Create 1e.py

---
+++ 1e.py
@@ -0,0 +1,14 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+a1= x & y #Bitwise AND 
+a2= x | y #Bitwise OR 
+a3= ~x #Bitwise NOT 
+a4=x ^ y # Bitwise XOR 
+a5=x >> 2 # >> Bitwise right shift 
+a6=x<<2 #<<Bitwise left shift 
+print( "bitwise AND=",a1) 
+print( "bitwise OR=",a2) 
+print( "bitwise NOT=",a3) 
+print( "bitwise XOR=",a4) 
+print( "bitwise Right shift=",a5) 
+print( "bitwise Left shift=",a6) 
