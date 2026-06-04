83de3d630bdb4fa72f7a50e5a7e5a13333f7e683
Pavan C N <p0073100@gmail.com>
2026-06-04 14:33:55 +0530

    Create 1g.py

---
+++ 1g.py
@@ -0,0 +1,9 @@
+# Program to check membership in a list
+
+numbers = [10, 20, 30, 40, 50]
+
+# Check if 20 is in the list
+print("Is 20 in list?", 20 in numbers)
+
+# Check if 60 is not in the list
+print("Is 60 not in list?", 60 not in numbers)
