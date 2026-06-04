792464c6505cdea008432154e94837714729f0c4
Pavan C N <p0073100@gmail.com>
2026-06-04 14:31:42 +0530

    Create 1d.py

---
+++ 1d.py
@@ -0,0 +1,13 @@
+# Program: Relational and Logical Operators
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print((a > 2) and (b >= 6)) 
+# logical AND 
+print(True and True) # True 
+print(True and False) # False 
+# logical OR 
+print(True or False) # True 
+# logical NOT 
+print(not True) # False
