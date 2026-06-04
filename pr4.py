832d6f8f1d20ea2b08e3c7d693a3613fe793dde6
Pavan C N <p0073100@gmail.com>
2026-06-04 14:41:33 +0530

    Create pr4.py

---
+++ pr4.py
@@ -0,0 +1,9 @@
+num=int(input("Enter the number"))
+print(f"multiple of num :-")
+for i in range(1,11):
+    print(f"{num*i}")
+    
+count= 44
+print(f"count = {count}")
+count+=44
+print(f"count = {count}")
