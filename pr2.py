4bc695602f495c46b5d2b6231294058803cba4f3
Pavan C N <p0073100@gmail.com>
2026-06-04 14:40:38 +0530

    Create pr2.py

---
+++ pr2.py
@@ -0,0 +1,13 @@
+print(f"enter length and bredth")
+length = float(input("Enter lenth :"))
+bredth = float(input("Enter bredth :"))
+print(f"Area is :- {length*bredth}")
+
+print(f"loan amount")
+amount = float(input("amount wanted $ :"))
+intrest_rate = float(input("intrest rate % :"))
+print(f"intrest amount : ${amount*intrest_rate/100}")
+
+num1=float(input("Enter number 1 :"))
+num2=float(input("Enter number 2 :"))
+print(f"addition: {num1+num2} , subtraction: {num1-num2} , multiplication: {num1*num2} , division: {num1/num2} ")
