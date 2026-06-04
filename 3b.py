337094495996460af3025f87c45bef1d34f3d872
Pavan C N <p0073100@gmail.com>
2026-06-04 14:36:23 +0530

    Create 3b.py

---
+++ 3b.py
@@ -0,0 +1,6 @@
+# Program to classify a number as positive, negative, or zero.using if-else
+num = int(input("Enter a number: "))
+if num >= 0:
+    print("Non-negative number")
+else:
+    print("Negative number")
